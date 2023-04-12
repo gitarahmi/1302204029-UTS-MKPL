1a2ec52dcfc56d4f6efc9a9704cda0af9185d7d2
gitarahmi <gitarahmifajrin@student.telkomuniversity.ac.id>
2023-04-12 22:04:28 +0700

    menambah untuk large class

--- lib/Employee.java
+++ lib/Employee.java
@@ -2,6 +2,8 @@ package lib;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.YearMonth;
+import java.time.temporal.TemporalAccessor;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
@@ -33,15 +35,13 @@ public class Employee {
 	private List<String> childNames;
 	private List<String> childIdNumbers;
 	
-	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
+	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, Date joinDate, boolean isForeigner, Gender gender) {
 		this.employeeId = employeeId;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.idNumber = idNumber;
 		this.address = address;
-		this.yearJoined = yearJoined;
-		this.monthJoined = monthJoined;
-		this.dayJoined = dayJoined;
+		this.joinDate = joinDate;
 		this.isForeigner = isForeigner;
 		this.gender = gender;
 		
@@ -54,21 +54,21 @@ public class Employee {
 	 * Jika pegawai adalah warga negara asing gaji bulanan diperbesar sebanyak 50%
 	 */
 	
-	public void setMonthlySalary(int grade) {	
+	public void hitungMonthlySalary(int grade) {	
 		if (grade == 1) {
-			monthlySalary = 3000000;
+			salary = 3000000;
 			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
+				salary = (int) (3000000 * 1.5);
 			}
 		}else if (grade == 2) {
-			monthlySalary = 5000000;
+			salary = 5000000;
 			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
+				salary = (int) (3000000 * 1.5);
 			}
 		}else if (grade == 3) {
-			monthlySalary = 7000000;
+			salary = 7000000;
 			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
+				salary = (int) (3000000 * 1.5);
 			}
 		}
 	}
@@ -93,15 +93,11 @@ public class Employee {
 	
 	public int getAnnualIncomeTax() {
 		
-		//Menghitung berapa lama pegawai bekerja dalam setahun ini, jika pegawai sudah bekerja dari tahun sebelumnya maka otomatis dianggap 12 bulan.
-		LocalDate date = LocalDate.now();
+	YearMonth currentYearMonth = YearMonth.now();
+	YearMonth joinYearMonth = YearMonth.from((TemporalAccessor) joinDate);
+	int monthsWorkedInCurrentYear = currentYearMonth.getMonthValue() - joinYearMonth.getMonthValue();
+	int monthsWorkedInTotal = (currentYearMonth.getYear() - joinYearMonth.getYear()) * 12 + monthsWorkedInCurrentYear;
 		
-		if (date.getYear() == yearJoined) {
-			monthWorkingInYear = date.getMonthValue() - monthJoined;
-		}else {
-			monthWorkingInYear = 12;
-		}
-		
-		return TaxFunction.calculateTax(monthlySalary, otherMonthlyIncome, monthWorkingInYear, annualDeductible, spouseIdNumber.equals(""), childIdNumbers.size());
+		return TaxFunction.calculateTax(salary, otherMonthlyIncome, monthsWorkedInTotal, annualDeductible, spouseIdNumber.equals(""), childIdNumbers.size());
 	}
 }
