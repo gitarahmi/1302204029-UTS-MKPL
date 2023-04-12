9eb62e09d184121fa00e44939ece9dd3db977329
gitarahmi <gitarahmifajrin@student.telkomuniversity.ac.id>
2023-04-12 18:46:01 +0700

    merubah long parameter pada class Employee

--- lib/Employee.java
+++ lib/Employee.java
@@ -7,6 +7,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Employee {
+	
 
 	private String employeeId;
 	private String firstName;
@@ -33,15 +34,13 @@ public class Employee {
 	private List<String> childNames;
 	private List<String> childIdNumbers;
 	
-	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
+	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, Date joinDate, boolean isForeigner, Gender gender) {
 		this.employeeId = employeeId;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.idNumber = idNumber;
 		this.address = address;
-		this.yearJoined = yearJoined;
-		this.monthJoined = monthJoined;
-		this.dayJoined = dayJoined;
+		this.joinDate = joinDate;
 		this.isForeigner = isForeigner;
 		this.gender = gender;
 		
